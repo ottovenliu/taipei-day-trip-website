0bd9330d860101c106fbaf9fd8392bdef9d963c1
ottovenliu <76770643+ottovenliu@users.noreply.github.com>
2021-04-20 09:11:45 +0800

    Backend0419_0.2.0

--- data/save_in_mysql.py
+++ data/save_in_mysql.py
@@ -1,9 +1,10 @@
 # 資料整理
+import mysql.connector
 import json
 file = open(
     'data/taipei-attractions.json', "r", encoding="utf-8")
 rawdata_json = json.load(file)
-print(len(rawdata_json["result"]["results"]))
+
 
 clist = []
 
@@ -11,14 +12,17 @@ for item in rawdata_json["result"]["results"]:
     images = []
     img_url = item["file"].split("http:")
     for url in img_url:
-        if ".jpg" or ".JPG" in url:
+        if (".jpg" in url or ".JPG" in url):
             images.append("http:"+url)
-        elif".png" or ".PNG" in url:
+
+        elif(".png" in url or ".PNG" in url):
             images.append("http:"+url)
+
         else:
             images.append("null")
             images.remove("null")
     dict = {
+        "id": item["_id"],
         "name": item["stitle"],
         "category": item["CAT1"]+","+item["CAT2"],
         "description": item["xbody"],
@@ -30,5 +34,23 @@ for item in rawdata_json["result"]["results"]:
         "images": images
     }
     clist.append(dict)
-print(clist)
-# print(json.dumps(rawdata_json, indent=3, ensure_ascii=False))
+
+
+# 輸入至mysql
+mydb = mysql.connector.Connect(
+    host="localhost",
+    user="my_user",
+    password="123456789",
+    database="my_db"
+)
+print(mydb)
+mycursor = mydb.cursor()
+for item in clist:
+    image = "".join(item["images"])
+
+    rawdata_sql = "INSERT INTO taipei_travel (web_id,name,category,description,address,transport,mrt,latitude,longitude,imges) VALUES(%s,%s, %s, %s,%s, %s, %s,%s, %s, %s)"
+    val = (item["id"], item["name"], item["category"], item["description"], item["address"],
+           item["transport"], item["mrt"], item["latitude"], item["longitude"], image)
+    mycursor.execute(rawdata_sql, val)
+    mydb.commit()
+    print(mycursor.rowcount, "was inserted")
