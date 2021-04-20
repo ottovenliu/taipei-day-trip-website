1dffc09951719a793309eb960a5842080350f371
ottovenliu <76770643+ottovenliu@users.noreply.github.com>
2021-04-20 20:04:24 +0800

    Backend0419_1.3.0

--- app.py
+++ app.py
@@ -27,34 +27,38 @@ def attraction(id):
         id=id)
     mycursor.execute(sql_page)
     myresult_page = mycursor.fetchall()
-
+    # error stear
     # 數據整理
-    data = []
-    for item in myresult_page:
-        dic = {
-            "id": item[0],
-            "name": item[1],
-            "category": item[2],
-            "description": item[3],
-            "address": item[4],
-            "transport": item[5],
-            "mrt": item[6],
-            "latitude": item[7],
-            "longitude": item[8],
-            "images": item[9]
-        }
-        data.append(dic)
+    print(myresult_page)
+    print(type(myresult_page))
+    dic = {
+        "id": myresult_page[0][0],
+        "name": myresult_page[0][1],
+        "category": myresult_page[0][2],
+        "description": myresult_page[0][3],
+        "address": myresult_page[0][4],
+        "transport": myresult_page[0][5],
+        "mrt": myresult_page[0][6],
+        "latitude": myresult_page[0][7],
+        "longitude": myresult_page[0][8],
+        "images": myresult_page[0][9]
+    }
+
     # 輸出
-    if len(data) == 0:
-        data = "無檢索資料"
+    if dic["id"] == False:
         abort(400)
     printout = {
-        "data": data
+        "data": dic
     }
     # TPtravelInfo_json = json.dumps(
     #     printout, ensure_ascii=False)
-    TPtravelInfo_json = json.dumps(printout)
-    return render_template("attraction.html", content_template=TPtravelInfo_json)
+
+    # TPtravelInfo_json = jsonify(printout)
+    # TPtravelInfo_json = json.dumps(printout)
+    # return render_template("attraction.html", content_template=TPtravelInfo_json)
+    return jsonify(printout)
+    # 跟json風磚的時候不太相關 重新試試看資料整理的方法有關 formater
+    # error end
 
 
 @app.route("/booking")
