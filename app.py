6377dccc2ba32663681417ca24a9f794392fa776
ottovenliu <76770643+ottovenliu@users.noreply.github.com>
2021-04-22 17:19:15 +0800

    Backend0419_1.5.4

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def attraction(id):
     mydb = mysql.connector.Connect(
         host="localhost",
         user="my_user",
-        password="123456789",
+        password="nhAG*nn8Yu7V",
         database="my_db"
     )
     # 數據庫查詢
@@ -73,7 +73,7 @@ def attractions():
     mydb = mysql.connector.Connect(
         host="localhost",
         user="my_user",
-        password="123456789",
+        password="nhAG*nn8Yu7V",
         database="my_db"
     )
     # 參數整理
